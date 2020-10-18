a3d6e32bc87efbcd9bdb22224da2cb5be7bb067d
Emanuel Zepeda <aezepedam@alumno.uspg.edu.gt>
2020-10-17 22:33:34 -0600

    Final de proyecto

--- src/uspg/edu/Registro_Factura.java
+++ src/uspg/edu/Registro_Factura.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
+ * Adolfo Emanuel Zepeda Medina 2000135
+ * 
  */
 package uspg.edu;
 
@@ -18,6 +18,7 @@ public class Registro_Factura {
     /**
      * @param args the command line arguments
      */
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
         Scanner input = new Scanner ( System.in);
         
